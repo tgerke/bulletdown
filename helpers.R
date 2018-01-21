b10cdac48c9676b39d7bdeda85ff4cfde9dd1cb9
Travis Gerke <tgerke@hsph.harvard.edu>
2018-01-20 21:29:07 -0500

    correct scoping problem for data frame in addFuture

--- helpers.R
+++ helpers.R
@@ -1,12 +1,11 @@
 # helper functions for adding/manipulating entries
 
 # specify date as month-day-year within the variety of formats lubridate will understand
-addFuture <- function(date, content=NULL, bullet=NULL, entries=entries) {
-  entries <- entries %>% add_row(date=mdy(date),
+addFuture <- function(date, content=NA, bullet=NA, data=entries) {
+  entries <<- data %>% add_row(date=mdy(date),
                                  month=months(date, abbreviate=TRUE),
                                  day=substr(weekdays(date), 1, 1),
                                  type="futurelog",
                                  content=content,
                                  bullet=bullet)
-  assign("entries", entries, .GlobalEnv)
 }
