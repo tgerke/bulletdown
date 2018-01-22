18ef9f3630cb23ae454dd8bd4d63eb8528aaeb56
Travis Gerke <tgerke@hsph.harvard.edu>
2018-01-21 21:46:10 -0500

    create tomorrow fuction and functions to add future/monthly/daily log entries in helpers.R

--- helpers.R
+++ helpers.R
@@ -1,11 +1,30 @@
 # helper functions for adding/manipulating entries
 
-# specify date as month-day-year within the variety of formats lubridate will understand
-addFuture <- function(date, content=NA, bullet=NA, data=entries) {
-  entries <<- data %>% add_row(date=mdy(date),
-                                 month=months(date, abbreviate=TRUE),
-                                 day=substr(weekdays(date), 1, 1),
-                                 type="futurelog",
-                                 content=content,
-                                 bullet=bullet)
+# create a function for tomorrow, much like lubridate's today()
+tomorrow <- function() {today()+1}
+
+# create generic entry-adding function
+# specify date as year-month-day within the variety of formats lubridate will understand
+# note that function expects a global data frame named "entries"
+addEntry <- function(date, content=NA, bullet=NA, data=entries, type=NA) {
+  entries <<- data %>% add_row(date=ymd(date),
+                               month=months(date, abbreviate=TRUE),
+                               day=weekdays(date, abbreviate=TRUE),
+                               type=type,
+                               content=content,
+                               bullet=bullet)
 }
+
+# add entries to future log
+addFuture <- function(date, content=NA, bullet=NA, data=entries, type) {
+  addEntry(date, content, bullet, data, type="futurelog")
+}
+# add entries to monthly log
+addMonthly <- function(date, content=NA, bullet=NA, data=entries, type) {
+  addEntry(date, content, bullet, data, type="monthlylog")
+}
+# add entries to daily log
+addDaily <- function(date, content=NA, bullet=NA, data=entries, type) {
+  addEntry(date, content, bullet, data, type="dailylog")
+}
+
